b97891d080ce378d9b24bd131d20a7a17eab0cfd
Trey Thomas <TreyThomasCodes@users.noreply.github.com>
2018-07-13 16:13:59 -0400

    Change assembly attributes

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NinjaTraderAddOnProject")]
+[assembly: AssemblyTitle("ZorroBridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NinjaTraderAddOnProject")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ZorroBridge")]
+[assembly: AssemblyCopyright("Copyright ©  2018 TreyThomasCodes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
